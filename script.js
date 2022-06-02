ab5e8a2bb2044ecd09abe5cec9bbd6a4112b054d
root <root@kali.local>
2022-06-02 16:27:51 +0200

    main

---
+++ script.js
@@ -0,0 +1,22 @@
+let typed = new Typed("#typing", {
+    strings: ["Beautiful Graphics", "Functional Websites", "Working Mobile Apps"],
+    typeSpeed: 150,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 1000,
+    fadeOut: false,
+    fadeIn: false,
+    loop: true,
+    showCursor: true,
+    autoInsertCss: false
+});
+
+window.onscroll = function () {
+    if (this.scrollY >= document.querySelector("nav").offsetTop) {
+        document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, .85)";
+    } else {
+        document.querySelector("nav").style.backgroundColor = "#0D0D0D";
+    }
+};
+
+new WOW().init();
